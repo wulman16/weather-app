948e2719d536d21c19af585e4886a8c210535d7d
wulman16 <will.ulman@gmail.com>
2019-05-24 17:21:20 -0400

    initialize mapbox functionality

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const request = require(`request`)
 require(`dotenv`).config()
 
-apiKey = process.env.API_KEY
+darkSkyApiKey = process.env.DARKSKY_API_KEY
+mapBoxApiKey = process.env.MAPBOX_API_KEY
 
-const url = `https://api.darksky.net/forecast/${apiKey}/33.7490000,-84.3879800`
+const darkSkyUrl = `https://api.darksky.net/forecast/${darkSkyApiKey}/33.7490000,-84.3879800`
+const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Atlanta.json?limit=1&access_token=${mapBoxApiKey}`
 
-request({ url: url, json: true }, (error, response) => {
+
+request({ url: darkSkyUrl, json: true }, (error, response) => {
   const currently = response.body.currently
   const summary = response.body.daily.data[0].summary
   console.log(`Today's forecast: ${summary}`)
   console.log(`It is currently ${currently.temperature} degrees outside. There is a ${currently.precipProbability}% chance of rain.`)
 })
+
+// Geocoding
+
+request({ url: mapBoxUrl, json: true }, (error, response) => {
+  const lat = response.body.features[0].center[1]
+  const lng = response.body.features[0].center[0]
+  const placeName = response.body.features[0].place_name
+  console.log(`${placeName} coordinates: (${lat}, ${lng})`)
+})
+
