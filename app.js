cb0c54d9fbe32042997f4a5a1a4d0d0425890c02
wulman16 <will.ulman@gmail.com>
2019-05-29 11:50:31 -0400

    integrate prettier

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-require(`dotenv`).config()
-const geocode = require(`./utils/geocode`)
-const weather = require(`./utils/weather`)
+/* eslint-disable no-undef */
+require(`dotenv`).config();
+const geocode = require(`./utils/geocode`);
+const weather = require(`./utils/weather`);
 
-DARKSKY_API_KEY = process.env.DARKSKY_API_KEY
-MAPBOX_API_KEY = process.env.MAPBOX_API_KEY
+DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
+MAPBOX_API_KEY = process.env.MAPBOX_API_KEY;
 
-const location = process.argv[2]
+const location = process.argv[2];
 
 if (location) {
   geocode(location, (error, geoData) => {
     if (error) {
-      return console.log(error)
+      return console.log(error);
     }
-  
+
     weather(geoData, (error, weatherData) => {
       if (error) {
-        return console.log(error)
+        return console.log(error);
       }
-      console.log(geoData.placeName)
-      console.log(weatherData)
-    })
-  })
+      console.log(geoData.placeName);
+      console.log(weatherData);
+    });
+  });
 } else {
-  console.log(`Please provide a location!`)
+  console.log(`Please provide a location!`);
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,20 +1,23 @@
-const request = require(`request`)
+/* eslint-disable no-undef */
+const request = require(`request`);
 
 const geocode = (address, callback) => {
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?limit=1&access_token=${MAPBOX_API_KEY}`
-  request({ url: url, json: true}, (error, {body}) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?limit=1&access_token=${MAPBOX_API_KEY}`;
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
-      callback(`Unable to connect to geolocation service!`)
+      callback(`Unable to connect to geolocation service!`);
     } else if (body.features.length === 0) {
-      callback(`Unable to find location! Try another search.`)
+      callback(`Unable to find location! Try another search.`);
     } else {
       callback(undefined, {
         lat: body.features[0].center[1],
         lng: body.features[0].center[0],
         placeName: body.features[0].place_name
-      })
+      });
     }
-  })
-}
+  });
+};
 
-module.exports = geocode
+module.exports = geocode;
